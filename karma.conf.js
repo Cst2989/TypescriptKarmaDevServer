1e7e28d9697ed10e59338cf1a5b5d8df39fd4f30
Neciu Dan <neciudan@Necius-MacBook-Pro.local>
2017-03-17 15:41:39 +0200

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+var webpackConfig = require('./webpack.config');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    files: [
+      'test/**/*.ts'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+      'test/**/*.ts': ['webpack']
+    },
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve
+    },
+    reporters: ['spec'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+module.exports = {
+    entry: {
+        app: ["./src/app.ts"]
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'ts-loader'
+        }
+      ]
+    }]
+  },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+};
