cb5828c94c04037e87e80eee81d4f14b2998fe88
Neciu Dan <neciudan@Necius-MacBook-Pro.local>
2017-03-27 10:19:02 +0300

    added css

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,49 @@
 import { Observable } from 'rxjs';
 
-var requestStream = Observable.of(3,4);
+var refreshButton = document.querySelector('.refresh');
 
-requestStream.subscribe(r => {
-    console.log(r)
+var refreshClick = Observable.fromEvent(refreshButton, 'click');
+
+var requestStream = Observable.of('https://api.github.com/users');
+
+var responseStream = requestStream.flatMap(response => {
+    return Observable.fromPromise(fetch(response).then(r => r.json()));
 })
+
+function createSuggestionStream(responseStream) {
+  return responseStream.map(listUser =>
+    listUser[Math.floor(Math.random()*listUser.length)]
+  );
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream);
+var suggestion2Stream = createSuggestionStream(responseStream.filter(r => r != suggestion1Stream));
+var suggestion3Stream = createSuggestionStream(responseStream.filter(r => r != suggestion2Stream));
+
+// Rendering ---------------------------------------------------
+function renderSuggestion(suggestedUser, selector) {
+  var suggestionEl = document.querySelector(selector);
+  if (suggestedUser === null) {
+    suggestionEl.style.visibility = 'hidden';
+  } else {
+    suggestionEl.style.visibility = 'visible';
+    var usernameEl = suggestionEl.querySelector('.username');
+    usernameEl.href = suggestedUser.html_url;
+    usernameEl.textContent = suggestedUser.login;
+    var imgEl = suggestionEl.querySelector('img');
+    imgEl.src = "";
+    imgEl.src = suggestedUser.avatar_url;
+  }
+}
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
